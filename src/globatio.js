3f2ff47a8fa0433d0382773145acd49b86b403ee
AntonioDanneck <49686988+AntonioDanneck@users.noreply.github.com>
2019-05-05 10:52:37 +0100

    Update globatio.js

--- src/globatio.js
+++ src/globatio.js
@@ -110,7 +110,7 @@ start(){
   sw.listen(port)
   console.log('Listening to port: ' + port)
  
-  sw.join('canjeitems-channel')
+  sw.join('globatio-channel')
 
   sw.on('connection', (conn, info) => {
     const seq = connSeq
@@ -502,13 +502,4 @@ var newstring=string.toLowerCase()
 
 return searchweight
 }
-// Human readable bytes util
-      function prettyBytes(num) {
-        var exponent, unit, neg = num < 0, units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-        if (neg) num = -num
-        if (num < 1) return (neg ? '-' : '') + num + ' B'
-        exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
-        num = Number((num / Math.pow(1000, exponent)).toFixed(2))
-        unit = units[exponent]
-        return (neg ? '-' : '') + num + ' ' + unit
-      }
+
